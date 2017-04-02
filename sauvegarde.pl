a9461cf10a14a3087038363f06761bbbdedf7a58
Émilie ROGER <emilie.roger@ensc.fr>
2017-04-02 15:57:48 +0200

    Initialisation de la sauvegarde

--- sauvegarde.pl
+++ sauvegarde.pl
@@ -1,9 +1,10 @@
 :- dynamic je_suis_a/1.
 
-je_suis_a(salle1).
+je_suis_a(entree).
 
 :- dynamic il_y_a/2.
 
+il_y_a(cle, salle1).
 il_y_a(torche, salle1).
 il_y_a(laissez-passer_W-51, salle2) :-
 	il_y_a(cle, en_main).
@@ -11,11 +12,10 @@ il_y_a(gourde, salle2) :-
 	il_y_a(cle, en_main).
 il_y_a(laissez-passer_R-24, salle6).
 il_y_a(laissez-passer_A-38, salle8).
-il_y_a(cle, en_main).
 
 :- dynamic je_possede/1.
 
-je_possede(1).
+je_possede(0).
 
 :- dynamic est_signe/1.
 
